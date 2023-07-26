39b8c6e1a52e1ab1315b730a55422bcec6340bb0
Alecadabra <27219575+Alecadabra@users.noreply.github.com>
2023-07-26 20:09:03 +0800

    Rename theme file

--- lib/bop.dart
+++ lib/bop.dart
@@ -1,6 +1,7 @@
 library bop;
 
 export 'package:bop/widgets/actions/button.dart';
+export 'package:bop/widgets/actions/icon_button.dart';
 export 'package:bop/widgets/text/content.dart';
 export 'package:bop/widgets/text/heading.dart';
 export 'package:bop/widgets/app.dart';
