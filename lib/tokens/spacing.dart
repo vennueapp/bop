0c5b9aa4874bca239aee2f98f5bb9f9e5a61b004
Alecadabra <27219575+Alecadabra@users.noreply.github.com>
2023-07-26 20:09:30 +0800

    Spacing constants

---
+++ lib/tokens/spacing.dart
@@ -0,0 +1,34 @@
+class BopSpacing {
+  /// `Spacing` x `0` = `0`
+  static const double x0 = 0;
+
+  /// `Spacing` x `1` = `4`
+  static const double x1 = 4;
+
+  /// `Spacing` x `2` = `8`
+  static const double x2 = 8;
+
+  /// `Spacing` x `3` = `12`
+  static const double x3 = 12;
+
+  /// `Spacing` x `4` = `16`
+  static const double x4 = 16;
+
+  /// `Spacing` x `5` = `20`
+  static const double x5 = 20;
+
+  /// `Spacing` x `8` = `32`
+  static const double x8 = 32;
+
+  /// `Spacing` x `10` = `40`
+  static const double x10 = 40;
+
+  /// `Spacing` x `15` = `60`
+  static const double x15 = 60;
+
+  /// `Spacing` x `16` = `64`
+  static const double x16 = 64;
+
+  /// Standard corner radius of `6` (off-grid)
+  static const double cornerRadius = 6;
+}
